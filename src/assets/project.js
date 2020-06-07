7f166d32bc6c9c361a991ac8eb322ab06b1a8275
astext <astext@gmail.com>
2020-06-07 14:00:40 +1000

    gradual migration 1

---
+++ src/assets/project.js
@@ -0,0 +1,16 @@
+/* checks if assets path is correct:  */
+//console.log("project.js is connected!") ;
+
+$(document).ready(function(){
+	$('.tooltip').tooltip() ;
+	$('.popover').popover();
+	
+	$('.nav-item.nav-link').on('click',function() {
+		$('div.navbar-collapse.collapse').removeClass('show');
+	});
+	
+	
+	
+});
+	
+	
